db3134a94adfcf5832e7fb4b2d99f27065c610a7
Alexander Chekhov <chekhovsasha4737@gmail.com>
2022-11-04 22:04:51 +0800

    =drom auto parses is close to be done

--- drom_ru/_test_script.py
+++ drom_ru/_test_script.py
@@ -3,73 +3,79 @@ import json
 import os
 import requests
 
+def collect_posts() -> list:
+  return [parse_post(link) for link in get_post_links()]
+  
 
-#container_selector = 'div.css-1173kvb.eaczv700 div.css-1nvf6xk.eaczv700'
-#title_selector = 'div.css-vsx4mr.e18vbajn0'
-#a_selector = 'a.css-xb5nz8.ewrty961'
-#
-#links = []
-#i = 1
-#while True:
-#  res = requests.get(f'https://kurchatov.drom.ru/auto/all/page{i}/?unsold=1')
-#  soup = bs4.BeautifulSoup(res.text, 'lxml')
-#  main_div = soup.select_one(container_selector)
-#  if main_div is None or main_div.select_one(title_selector) is not None:
-#    break
-#  new_links = [a.get('href') for a in main_div.select(a_selector)]
-#  
-#  print(f'page {i}')
-#  print(*new_links, sep='\n')
-#  print()
-#  links += new_links
-#  i += 1
+def get_post_links() -> list:
+  links = []
+  i = 1
+  while True:
+    res = requests.get(f'https://kurchatov.drom.ru/auto/all/page{i}/?unsold=1')
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+    main_div = soup.select_one('div.css-1173kvb.eaczv700 div.css-1nvf6xk.eaczv700')
+    if main_div is None or main_div.select_one('div.css-vsx4mr.e18vbajn0') is not None:
+      break
+    links += [a.get('href') for a in main_div.select('a.css-xb5nz8.ewrty961')]
+    i += 1
+  return links
 
 
+def parse_post(post_url:str) -> dict:
+  soup = bs4.BeautifulSoup(requests.get(post_url).text, 'lxml')
+  return {
+    "publishedAt": get_date(soup),
+    "text": get_params_text(soup)+'\n\n'+\
+            get_data_list_text(soup)+'\n\n'+\
+            get_additional_info(soup),
+    "city": get_city(soup),
+    "url": post_url,
+    "source": "drom.ru"
+  }
 
 
-# url = 'https://kurchatov.drom.ru/lada/2107/48584167.html'
-# file_path = os.path.join(os.path.dirname(__file__), 'static\\_temp.html')
-# page_text = requests.get(url).text
-# with open(file_path, 'w+') as f:
-#   f.write(page_text)
+def get_price(soup:bs4.BeautifulSoup):
+  return soup.select_one('div.css-eazmxc.e162wx9x0').text.replace('\xa0', ' ')
 
 
-file_path = os.path.join(os.path.dirname(__file__), 'static\\_temp.html')
-with open(file_path, 'r') as f:
-  page_text = f.read()
+def get_date(soup:bs4.BeautifulSoup):
+  return soup.select_one('div.css-pxeubi.evnwjo70').text.split()[-1]
 
-soup = bs4.BeautifulSoup(page_text, 'lxml')
 
-date_selector = 'div.css-pxeubi.evnwjo70'
-price_selector = 'div.css-eazmxc.e162wx9x0'
-param_names_selector = 'th.css-16lvhul.ezjvm5n1'
-param_values_selector = 'td.css-9xodgi.ezjvm5n0'
-
-def get_params_text():
+def get_params_text(soup:bs4.BeautifulSoup):
   res = []
-  for n, v in zip(soup.select(param_names_selector), soup.select(param_values_selector)):
+  param_names = soup.select('th.css-16lvhul.ezjvm5n1')
+  param_values = soup.select('td.css-9xodgi.ezjvm5n0')
+  for n, v in zip(param_names, param_values):
     if v.string is None:
       if v.find('span') is not None:
         v = v.find('span')
       else:
         v = v.find('a')
-    res.append(f'{n.text}: {v.text}')
+    v_text = v.text
+    if 'Мощ' in n.text:
+      v_text = v_text.split(',')[0]
+      v_text.replace('\xa0', ' ')
+    res.append(f'{n.text}: {v_text}')
   return '\n'.join(res)
 
-print(get_params_text())
 
-# print(*[(n.text, v.text) for n, v in zip(soup.select(param_names_selector),
-                              #  soup.select(param_values_selector))], sep='\n')
+def get_data_list_text(soup:bs4.BeautifulSoup):
+  data_list = soup.select('div.css-xvjsia.ei6sjku0 div')
+  return '\n'.join({div.text for div in data_list if 'green' in str(div)})  
 
-'''
-{
-  "id": "",
-  "userId": "",
-  "publishedAt": "",
-  "text": "",
-  "city": "",
-  "contacts": "",
-  "url": "",
-  "source": ""
-}
-'''
+
+def get_additional_info(soup:bs4.BeautifulSoup):
+  text_span = soup.select_one('div.css-inmjwf.e162wx9x0 span.css-1kb7l9z.e162wx9x0')
+  return '' if not text_span else text_span.text
+
+
+def get_city(soup:bs4.BeautifulSoup):
+  return soup.select('div.css-inmjwf.e162wx9x0')[-1].text.split(':')[-1].strip()
+
+
+
+if __name__ == '__main__':
+  posts = collect_posts()
+  with open(os.path.dirname(__file__)+'/static/_temp.json', 'w+', encoding='utf-8') as f:
+    json.dump(posts, f, ensure_ascii=False, indent=4)
