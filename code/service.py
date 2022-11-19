1439b678a7c6ee02e39a07fb33539f15cbd44527
Alexander Chekhov <chekhovsasha4737@gmail.com>
2022-11-19 16:19:33 +0800

    =Now aiohttp.ClientSession per site

--- code/service.py
+++ code/service.py
@@ -1,23 +1,32 @@
 import aiohttp
-from typing import Generator
 from .tools import CarType
 from .collecting import PostCollector, DromPostCollector
 import asyncio_atexit
+from typing import Callable, TypeVar, ParamSpec
+T, P = TypeVar('T'), ParamSpec('P')
 
 
 COLLECTOR_CLASSES:list = [DromPostCollector]
 COLLECTORS:list[PostCollector] = []
-SESSION:aiohttp.ClientSession = None
 
 
-async def get_posts(city:str, car_type:CarType) -> Generator[list[dict], tuple[str, CarType], None]:
-  global COLLECTORS, SESSION
-  if not SESSION:
-    SESSION = aiohttp.ClientSession()
-    asyncio_atexit.register(SESSION.close)
-    COLLECTORS = [cl(SESSION) for cl in COLLECTOR_CLASSES]
+def inited(func: Callable[P, T]) -> Callable[P, T]:
+  def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
+    if not COLLECTORS:
+      for cl in COLLECTOR_CLASSES:
+        session = aiohttp.ClientSession()
+        asyncio_atexit.register(session.close)
+        COLLECTORS.append(cl(session))
+    return func(*args, **kwargs)
+  return wrapper
+
+
+@inited
+async def get_posts(city:str, car_type:CarType) -> list[dict[str, str|dict[str, str]]]:
   response = []
   for collector in COLLECTORS:
     async for post in collector.collect_posts(city, car_type):
       response.append(post.as_dict())
   return response
+
+
